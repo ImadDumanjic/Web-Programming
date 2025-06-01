4557bf930eb808fd430e860a37a9b8827fbb34cf
Imad <imad.dumanjic@stu.ibu.edu.ba>
2025-06-01 21:30:43 +0200

    Deployment fixes

--- Frontend/utils/constants.js
+++ Frontend/utils/constants.js
@@ -1,5 +1,6 @@
 let Constants = {
-  PROJECT_BASE_URL: "http://localhost/project/backend/",
+  //PROJECT_BASE_URL: "http://localhost/project/backend/",
+  PROJECT_BASE_URL: "https://luxury-drive-frontent-jubf7.ondigitalocean.app/",
   CUSTOMER_ROLE: "Customer", 
   ADMIN_ROLE: "Admin"
 };
