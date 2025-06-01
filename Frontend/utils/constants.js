60958225acaaa386a585603055b3cc607951965b
Imad <imad.dumanjic@stu.ibu.edu.ba>
2025-06-01 21:41:45 +0200

    Fixes

--- Frontend/utils/constants.js
+++ Frontend/utils/constants.js
@@ -1,6 +1,6 @@
 let Constants = {
   //PROJECT_BASE_URL: "http://localhost/project/backend/",
-  PROJECT_BASE_URL: "https://luxury-drive-frontent-jubf7.ondigitalocean.app/",
+  PROJECT_BASE_URL: "https://luxury-drive-2w37n.ondigitalocean.app/index.php/",
   CUSTOMER_ROLE: "Customer", 
   ADMIN_ROLE: "Admin"
 };
