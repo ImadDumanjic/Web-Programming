f2de9c4833db1f81befc3d5db1175ea47f1b57ac
Imad <imad.dumanjic@stu.ibu.edu.ba>
2025-06-01 21:55:51 +0200

    Fixes

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -7,18 +7,18 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 require __DIR__ . '/../../../vendor/autoload.php';
 
 define('LOCALSERVER', 'http://localhost/project/backend/');
-define('PRODSERVER', 'https://add-production-server-after-deployment/backend/');
+define('PRODSERVER', 'https://luxury-drive-2w37n.ondigitalocean.app/index.php/');
 
-if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
-    define('BASE_URL', 'http://localhost/project/backend/');
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    define('BASE_URL', LOCALSERVER);
 } else {
-    define('BASE_URL', 'https://add-production-server-after-deployment/backend/');
+    define('BASE_URL', PRODSERVER);
 }
 
 $openapi = \OpenApi\Generator::scan([
     __DIR__ . '/doc_setup.php',
     __DIR__ . '/../../../Rest/Routes'
 ]);
+
 header('Content-Type: application/json');
 echo $openapi->toJson();
-?>
