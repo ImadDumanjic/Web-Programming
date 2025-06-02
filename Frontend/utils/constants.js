1dbdc4cb38ef66540b4ec05316d4b46c8f48618a
Imad <imad.dumanjic@stu.ibu.edu.ba>
2025-06-02 12:26:04 +0200

    Fixes

--- Frontend/utils/constants.js
+++ Frontend/utils/constants.js
@@ -2,8 +2,8 @@ let Constants = {
   //PROJECT_BASE_URL: "http://localhost/project/backend/",
   PROJECT_BASE_URL:
     location.hostname === "localhost"
-      ? "http://localhost:83/project/backend/"
-      : "https://luxury-drive-2w37n.ondigitalocean.app/index.php/",
+      ? "http://localhost/project/backend/"
+      : "https://luxury-drive-2w37n.ondigitalocean.app/backend/",
   CUSTOMER_ROLE: "Customer", 
   ADMIN_ROLE: "Admin"
 };
